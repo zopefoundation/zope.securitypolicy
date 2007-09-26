ec214af2333bde54787000092afd0d80a9d22425
Stephan Richter <stephan.richter@gmail.com>
2007-09-26 23:26:20 +0000

    Oops, wrong package name.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-setup(name='zope.app.securitypolicy',
+setup(name='zope.securitypolicy',
     version = '3.4.0',
     author='Zope Corporation and Contributors',
     author_email='zope3-dev@zope.org',
