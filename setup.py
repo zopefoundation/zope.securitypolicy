2c4957c75e86e3386bd97be75f256282fea0d916
Martijn Faassen <faassen@startifact.com>
2009-01-31 14:11:02 +0000

    Release preps.

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zope.securitypolicy',
-    version = '3.5.0dev',
+    version = '3.5.0',
     author='Zope Corporation and Contributors',
     author_email='zope-dev@zope.org',
     description='Default security policy for Zope3',
