5640d57f1ce59a3df3ab8d839685c3f062d62a0d
Michael Howitz <mh@gocept.com>
2010-09-25 11:57:19 +0000

    Preparing release 3.7.0

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zope.securitypolicy',
-    version='3.7.0dev',
+    version='3.7.0',
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.org',
     description='Default security policy for Zope3',
