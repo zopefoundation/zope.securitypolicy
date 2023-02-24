60870eb2efba492fbb5adfab2e089238344486a1
Michael Howitz <mh@gocept.com>
2023-02-24 08:10:29 +0100

    Back to development: 5.1

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ def read(*rnames):
 
 setup(
     name='zope.securitypolicy',
-    version='5.0',
+    version='5.1.dev0',
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.dev',
     description='Default security policy for Zope3',
